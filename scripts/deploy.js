93a8d17a3552b7ea7f38fcd5cd2819d4c1456ee8
Swati Jindal <88382946+Swati2701@users.noreply.github.com>
2021-09-30 20:58:32 +0530

    Initial Commit

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    console.log('Deploying contracts with the account:', deployer.address);
+
+    console.log('Account balance:', (await deployer.getBalance()).toString());
+
+     const Token= await ethers.getContractFactory("MyTokenSale")
+      
+        // Start deployment, returning a promise that resolves to a contract object
+     const tokenCreation = await Token.deploy()
+     console.log("Contract deployed to address:", tokenCreation.address)
+}
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+    console.error(error)
+    process.exit(1)
+})
